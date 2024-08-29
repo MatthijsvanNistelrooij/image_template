624df5a1322116fe682c05f901f12d60f1330c60
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-08-29 11:20:51 +0200

    test deploy

--- app/api/images/route.js
+++ app/api/images/route.js
@@ -3,7 +3,7 @@ import Image from "@/models/Image";
 
 export async function GET() {
   await connectToDatabase();
-  const images = await Image.find().limit(10); // Limit the number of images
+  const images = await Image.find().limit(100); 
 
   return new Response(JSON.stringify(images), { status: 200 });
 }
